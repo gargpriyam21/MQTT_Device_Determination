e9b82e280f9a5f9492494ecde2dc219a9de617f6
Divyag Doshi <divyangdoshi02@gmail.com>
2022-03-09 13:51:46 -0500

    Adding code for RaspberryPiB

---
+++ RaspberryPiB/README.md
@@ -0,0 +1,40 @@
+# IoT_ASN3_G11 - MQTT Subscriber Only (Raspberry Pi B)
+
+This repository is created for the sole purpose of uploading codes related to the Assignment 3 for the course CSC 591 - 022 Internet of Things: Architectures, Applications, and Implementation Spring 2022 of North Carolina State University.
+
+## Environment
+- macOS Monterey Version 12.2.1
+- Python 3.7.3
+
+**VISHAL TO EXPLAIN THE HARDWARE ENVIRONMENT FOR RASPBERRY PI B**
+
+## Requirements
+### Software
+- Python3 3.7.3
+- paho-mqtt
+- mosquitto
+
+### Hardware
+**VISHAL TO EXPLAIN THE HARDWARE PART FOR RASPBERRY PI B**
+
+## Procedure
+Initially update the `BROKER_IP_ADDRESS` in the code file (*RPiB.py*) to the IP_ADDRESS of device where the Broker is currently running. If the broker is on the same device update the value to **'localhost'**
+Run the ***Raspberry Pi B (this will only be subscriber)*** code by executing the below command on the Raspberry Pi
+```
+python3 RPiB.py
+```
+
+**VISHAL TO EXPLAIN THE HARDWARE PROCEDURE FOR RASPBERRY PI B**
+
+# Instructor
+- Dr. Muhammad Shahzad (mshahza@ncsu.edu )
+
+# Teaching Assistants
+- Hassan Ali Khan (hakhan@ncsu.edu)
+
+# Team
+- Priyam Garg (pgarg6@ncsu.edu)
+- Divyang Doshi	(ddoshi2@ncsu.edu)
+- Brendan Driscoll (bhdrisco@ncsu.edu)
+- Jordan Boerger (jwboerge@ncsu.edu)
+- Vishal Veera Reddy (vveerar2@ncsu.edu)
---
+++ RaspberryPiB/RPiB.py
@@ -0,0 +1,83 @@
+import os
+import sys
+import time
+from datetime import datetime
+import statistics
+import collections
+from collections import defaultdict
+from paho.mqtt import client as mqtt
+
+# BROKER IP: Need to change if the broker is changed
+BROKER_IP_ADDRESS = 'localhost'
+PORT = 1883
+KEEPALIVE = 200
+MQTT_TOPICS = [("ncsu/iot/G11/LightStatus",2), ("ncsu/iot/G11/Status/RaspberryPiA",2), ("ncsu/iot/G11/Status/RaspberryPiC",2)]
+
+OFF = 0
+ON = 1
+
+LED1 = OFF
+LED2 = OFF
+LED3 = OFF
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to the Broker with result code "+str(rc))
+
+def on_message(client, userdata, message):
+    global LED1, LED2, LED3
+    TOPIC = message.topic
+    QOS = message.qos
+
+    if TOPIC == MQTT_TOPICS[0][0]:
+        MESSAGE_SENT = str(message.payload.decode("utf-8"))
+        if MESSAGE_SENT.lower() == "turnoff":
+            LED1 = OFF
+        elif MESSAGE_SENT.lower() == "turnon":
+            LED1 = ON
+    elif TOPIC == MQTT_TOPICS[1][0]:
+        MESSAGE_SENT = str(message.payload.decode("utf-8"))
+        if MESSAGE_SENT.lower() == "offline":
+            LED2 = OFF
+        elif MESSAGE_SENT.lower() == "online":
+            LED2 = ON
+    elif TOPIC == MQTT_TOPICS[2][0]:
+        MESSAGE_SENT = str(message.payload.decode("utf-8"))
+        if MESSAGE_SENT.lower() == "offline":
+            LED3 = OFF
+        elif MESSAGE_SENT.lower() == "online":
+            LED3 = ON
+    
+    BlinkLEDS()
+
+def BlinkLEDS():
+    """
+    # @VISHAL VEERA READY
+    Plese complete this pat of the Raspberry pi
+    Blink the LED's accordingly to the values of LED1, LED2 and LED3
+    """
+    # print("LED LightStatus: " + str(LED1))
+    # print("LED2 RPiA: " + str(LED2))
+    # print("LED3 RPiC: " + str(LED3))
+    # print("\n" + "-"*50 + "\n")
+    pass
+
+
+def run():
+    
+    subscriber = mqtt.Client("RPiB")
+    
+    subscriber.on_connect = on_connect
+    subscriber.on_message = on_message
+
+    subscriber.connect(host = BROKER_IP_ADDRESS, port = PORT, keepalive = KEEPALIVE)
+
+    subscriber.loop_start()
+
+    subscriber.subscribe(MQTT_TOPICS)
+
+    while True:
+        continue
+
+
+if __name__ == '__main__':
+    run()
