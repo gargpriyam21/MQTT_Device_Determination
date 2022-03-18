0f3853105294df602aa3b74b1e11888073b45652
Priyam Garg <gargpriyam21@gmail.com>
2022-03-18 03:25:38 -0400

    updating RPiC code to handle concurrency

--- RaspberryPiC/RPiC.py
+++ RaspberryPiC/RPiC.py
@@ -5,12 +5,13 @@ from datetime import datetime
 import statistics
 import collections
 from collections import defaultdict
+from collections import deque
 from paho.mqtt import client as mqtt
 
 # BROKER IP: Need to change if the broker is changed
-BROKER_IP_ADDRESS = 'localhost'
-PORT = 1883
-KEEPALIVE = 200
+BROKER_IP_ADDRESS = '107.13.179.1'
+PORT = 3276
+KEEPALIVE = 60
 
 MQTT_SUBSCRIBE_TOPICS = [("ncsu/iot/G11/lightSensor",2), ("ncsu/iot/G11/threshold",2), ("ncsu/iot/G11/LightStatus",2)]
 MQTT_PUBLISH_TOPICS = [("ncsu/iot/G11/LightStatus",2), ("ncsu/iot/G11/Status/RaspberryPiC",2)]
@@ -25,6 +26,10 @@ LIGHTSTATUS = OFF
 
 LAST_WILL_MESSAGE = "offline"
 
+LDR_Q = deque()
+POTEN_Q = deque()
+PREV_SET = True
+
 def on_connect(client, userdata, flags, rc):
     topic = MQTT_PUBLISH_TOPICS[1][0]
     qos = MQTT_PUBLISH_TOPICS[1][1]
@@ -38,37 +43,61 @@ def on_disconnect(client, userdata, flags, rc):
     print("Disconnected to the Broker with result code "+str(rc))
 
 def on_message(client, userdata, message):
-    global LIGHTSTATUS, PREVIOUS_LIGHTSTATUS, THRESHOLD, LDR_VALUE
+    global LIGHTSTATUS, PREVIOUS_LIGHTSTATUS, THRESHOLD, LDR_VALUE, LDR_Q, POTEN_Q, PREV_SET
 
     TOPIC = message.topic
     QOS = message.qos
 
     if TOPIC == MQTT_SUBSCRIBE_TOPICS[0][0]:
         MESSAGE = str(message.payload.decode("utf-8"))
-        LDR_VALUE = int(MESSAGE)
+        # LDR_VALUE = float(MESSAGE)
+        LDR_Q.append(float(MESSAGE))
 
     elif TOPIC == MQTT_SUBSCRIBE_TOPICS[1][0]:
         MESSAGE = str(message.payload.decode("utf-8"))
-        THRESHOLD = int(MESSAGE)
-
-        if THRESHOLD is not None and LDR_VALUE is not None:
-            calculate_lightstatus(client)
+        # THRESHOLD = float(MESSAGE)
+        POTEN_Q.append(float(MESSAGE))
 
     elif TOPIC == MQTT_SUBSCRIBE_TOPICS[2][0]:
         MESSAGE = str(message.payload.decode("utf-8"))
         PREVIOUS_LIGHTSTATUS = MESSAGE
-    
+        PREV_SET = True
+
+    if LDR_Q and POTEN_Q and len(LDR_Q) == len(POTEN_Q) and PREV_SET:
+        calculate_lightstatus(client)
+
 def calculate_lightstatus(client):
 
-    if LDR_VALUE >= THRESHOLD:
-        LIGHTSTATUS = ON
-    else:
-        LIGHTSTATUS = OFF
-    
-    if LIGHTSTATUS != PREVIOUS_LIGHTSTATUS:
-        topic = MQTT_PUBLISH_TOPICS[0][0]
-        qos = MQTT_PUBLISH_TOPICS[0][1]
-        client.publish(topic = topic, payload = LIGHTSTATUS, qos = qos, retain = True)
+    global LIGHTSTATUS, PREVIOUS_LIGHTSTATUS, THRESHOLD, LDR_VALUE, LDR_Q, POTEN_Q, PREV_SET
+
+    ONE_PUBLISHED = False
+    while LDR_Q and POTEN_Q:
+        print("LDR")
+        print(LDR_Q)
+
+        print("Poten")
+        print(POTEN_Q)
+
+        print("Light Status = " + LIGHTSTATUS)
+        print("Prev Light Status = " + PREVIOUS_LIGHTSTATUS)
+
+        print("*"*50)
+
+        LDR_VALUE = LDR_Q.popleft()
+        THRESHOLD = POTEN_Q.popleft()
+        if LDR_VALUE >= THRESHOLD:
+            LIGHTSTATUS = ON
+        else:
+            LIGHTSTATUS = OFF
+        
+        if LIGHTSTATUS != PREVIOUS_LIGHTSTATUS:
+            topic = MQTT_PUBLISH_TOPICS[0][0]
+            qos = MQTT_PUBLISH_TOPICS[0][1]
+            ONE_PUBLISHED = True
+            client.publish(topic = topic, payload = LIGHTSTATUS, qos = qos, retain = True)
+            break
+    if ONE_PUBLISHED:
+        PREV_SET = False
 
 def run():
     
